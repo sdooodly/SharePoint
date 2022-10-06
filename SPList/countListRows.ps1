cdbf4694fcd0dce36dadc2d93c7c10083c1029c5
Sdoodly <91261240+sdooodly@users.noreply.github.com>
2022-10-06 17:03:57 +0530

    Update countListRows.ps1

--- SPList/countListRows.ps1
+++ SPList/countListRows.ps1
@@ -7,3 +7,8 @@ Connect-PnPOnline $SiteURL -Credentials (Get-Credential)
  
 #Get List Item count from all Lists from the Web
 Get-PnPList | Select Title, ItemCount
+
+#in case of MFA issues use this
+$SiteURL= "//required site url//"
+Connect-PnPOnline $SiteURL -UseWebLogin
+Get-PnPList | Select Title, ItemCount
